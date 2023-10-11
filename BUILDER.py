ae5e877ddc28d7abf021b9afe508dba76565dd77
Illumi14 <145398159+Illumi14@users.noreply.github.com>
2023-10-11 19:14:10 +0100

    Add files via upload

---
+++ BUILDER.py
@@ -0,0 +1,8 @@
+import subprocess
+#installs dependencies
+subprocess.Popen("pip install dhooks")
+subprocess.Popen("pip install customtkinter")
+
+#creates payload exe
+subprocess.Popen("pyinstaller --onefile asdasdasd.py")
+
