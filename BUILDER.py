28ce9b944ae66b2e5824a9d627b5577e91c8cf4b
Illumi14 <145398159+Illumi14@users.noreply.github.com>
2023-12-19 21:47:25 +0000

    Delete BUILDER.py
    reworking

--- BUILDER.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-#installs dependencies
-subprocess.Popen("pip install dhooks")
-subprocess.Popen("pip install customtkinter")
-
-#creates payload exe
-subprocess.Popen("pyinstaller --onefile asdasdasd.py")
-
